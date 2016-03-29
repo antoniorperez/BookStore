278d3a7876d0974a7d4659741f4a5e7d781b0854
antoniorperez <antoniorperez85@gmail.com>
2016-03-29 22:26:58 +0200

    First Commit

--- BookStore/ViewController.swift
+++ BookStore/ViewController.swift
@@ -8,10 +8,31 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController ,UITextFieldDelegate{
+    
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var textView: UITextView!
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {   //delegate method
+        
+        let urls = "https://openlibrary.org/api/books?jscmd=data&format=json&bibkeys=ISBN:" + textField.text!
+        let url = NSURL(string: urls)
+        let datos:NSData? = NSData(contentsOfURL: url!)
+        if(datos == nil){
+            textView.text="Error al conectar con el servidor"
+            textView.textColor = UIColor.redColor()
+        }else{
+            let texto = NSString(data:datos!, encoding:NSUTF8StringEncoding)
+            textView.text = texto as! String
+            textView.textColor = UIColor.blackColor()
+        }
+        textField.resignFirstResponder()
+        return true
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.textField.delegate = self
         // Do any additional setup after loading the view, typically from a nib.
     }
 
